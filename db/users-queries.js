882fbbc7e23bd73dc2160b2bd85803f3093d724f
Jerico <jerico.cabatingan@gmail.com>
2022-06-06 02:16:49 +0100

    user endpoints can serve requests and handle errors

--- db/users-queries.js
+++ db/users-queries.js
@@ -24,6 +24,77 @@ const submitNewUser = async (request, response) => {
   })
 };
 
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users;', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(results.rows);
+  })
+};
+
+
+const getUserById = (request, response) => {
+  const id = request.params.id
+  pool.query('SELECT * FROM users WHERE id = $1;',[id], (error, results) => {
+    if (error) {
+      throw error;
+    } 
+    else if (results.rows.length === 0 ) {
+      response.status(404).send("User does not exist");
+    } 
+    else if (results.rows.length > 0) {
+      response.status(200).json(results.rows[0]);
+    }
+  })
+};
+
+const updateUser = async (request, response) => {
+  const { username, email, password } = request.body; 
+
+  const paramId = request.params.id;
+  const result = request.idArray.filter(user => user.id === paramId);
+
+  let userId;
+  result.length === 0 ? userId === null : userId = result[0].id;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  pool.query('UPDATE users SET email = $1, username = $2, password = $3 WHERE id = $4;', [username, email, hashedPassword, userId], (error) => {
+    if (error) {
+      response.status(400).send('Invalid input')
+      throw error
+    } 
+    else if (userId) {
+      response.status(200).send(`User with ID: ${userId} was updated to:
+      email: ${email}
+      username: ${username}
+      password: ${hashedPassword}`);
+    }
+    else if (!userId) {
+      response.status(404).send(`User with ID: ${paramId} not found`)
+    };
+  })
+};
+
+// middleware for appending ids array to the request object
+const getIds = (request, response, next) => {
+  pool.query('SELECT id FROM users;', (error, results) => {
+    if (error) {
+      throw error
+    }
+    request.idArray = results.rows;
+    next();
+  })
+};
+
 module.exports = {
   submitNewUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  getIds
 };
+
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,10 @@ const usersRouter = express.Router();
 
 usersRouter.post('/register', dbQuery.submitNewUser);
 
+usersRouter.get('/', dbQuery.getUsers);
+
+usersRouter.get('/:id', dbQuery.getUserById);
+
+usersRouter.put('/:id',dbQuery.getIds ,dbQuery.updateUser);
+
 module.exports = usersRouter;
